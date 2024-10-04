6b2d47faa3a88134b641098990c2e7c11935546f
LucasHenriqueSouzaN <114408410+LucasHenriqueSouzaN@users.noreply.github.com>
2024-10-04 11:42:59 -0300

    Add files via upload

---
+++ Inicial/js.js
@@ -0,0 +1,10 @@
+// CARRINHO
+const cart =  document.getElementById('cart'),
+      cartShop = document.getElementById('cart-shop'),
+      cartClose = document.getElementById('cart-close')
+
+if(cartShop){
+    cartShop.addEventListener("click", () => {
+        cart.classList.add(show-cart)
+    })
+}
