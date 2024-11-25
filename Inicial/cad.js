7bbb32c7a95b5083bd1983dd8e5156fb92a8e3cc
Aline-svg <aline.santosde.souza@escola.pr.gov.br>
2024-11-25 20:10:43 -0300

    Add files via upload

--- Inicial/cad.js
+++ Inicial/cad.js
@@ -7,3 +7,4 @@ document.getElementById('open-login').addEventListener('click', moveOverlay)
 
 document.getElementById('open-register-mobile').addEventListener('click', moveOverlay)
 document.getElementById('open-login-mobile').addEventListener('click', moveOverlay)
+
